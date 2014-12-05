494b0025e50b4874da8826c3af3ae59024ce5297
Julien Petitbon <julien.petitbon@y-nov.com>
2014-12-05 03:25:05 +0100

    ActionBean.java

---
+++ ImHuman/app/src/main/java/imhuman/app/com/Data/ActionBean.java
@@ -0,0 +1,33 @@
+package imhuman.app.com.Data;
+
+/**
+ * Created by Julien on 05/12/2014.
+ */
+public class ActionBean {
+    private int id;
+    private String text, type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
