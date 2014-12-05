9caa964e44796510986ab9542ef0ef7a1bef2209
Julien Stefani <julien.stefani@y-nov.com>
2014-12-05 01:35:37 +0100

    GROS COMMISSION

--- ImHuman/app/src/main/java/imhuman/app/com/View/MainActivity.java
+++ ImHuman/app/src/main/java/imhuman/app/com/View/MainActivity.java
@@ -1,9 +1,13 @@
 package imhuman.app.com.View;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import imhuman.app.com.R;
 
@@ -14,6 +18,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btConnect = (Button) findViewById(R.id.Connexion);
+        btConnect.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Connexion.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btRegister = (Button) findViewById(R.id.Inscription);
+        btRegister.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Inscription.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btConvert = (Button) findViewById(R.id.Convertisseur);
+        btConvert.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Convertisseur.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
